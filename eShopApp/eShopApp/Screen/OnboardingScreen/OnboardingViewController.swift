cbbad12139c99091d69e4652ded55132758d9aee
Văn Khánh Vương <vankhanhvuong98@gmail.com>
2021-05-23 06:47:14 +0700

    fix: Connection file Onboarding.storyboard, OnboardingViewController.swift

---
+++ eShopApp/eShopApp/Screen/OnboardingScreen/OnboardingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingViewController.swift
+//  eShopApp
+//
+//  Created by Văn Khánh Vương on 17/05/2021.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
