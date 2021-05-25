155934b8827e7188fc3da16bfdbc2c90703fc626
Văn Khánh Vương <vankhanhvuong98@gmail.com>
2021-05-25 10:33:28 +0700

    feat: commit code UITable Cell Account Screen

---
+++ eShopApp/eShopApp/Data/Networking/Model/CategoryUser.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryUser.swift
+//  eShopApp
+//
+//  Created by Văn Khánh Vương on 25/05/2021.
+//
+
+import Foundation
+
+struct CategoryUser {
+    
+}
---
+++ eShopApp/eShopApp/Screen/AccountScreen/Part/Cell/AccountTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  AccountTableViewCell.swift
+//  eShopApp
+//
+//  Created by Văn Khánh Vương on 25/05/2021.
+//
+
+import UIKit
+
+class AccountTableViewCell: UITableViewCell {
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var nameItemLabel: UILabel!
+    
+}
