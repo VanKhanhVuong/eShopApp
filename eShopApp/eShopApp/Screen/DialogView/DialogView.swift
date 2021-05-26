1926291a4d67c1a345221217cc0e5cd1dbcdfa8d
Văn Khánh Vương <vankhanhvuong98@gmail.com>
2021-05-26 10:52:41 +0700

    feat : Commit Dialog View

---
+++ eShopApp/eShopApp/Screen/DialogView/DialogView.swift
@@ -0,0 +1,13 @@
+//
+//  DialogView.swift
+//  eShopApp
+//
+//  Created by Văn Khánh Vương on 26/05/2021.
+//
+
+import UIKit
+
+class DialogView: UIView {
+    @IBOutlet weak var tryAgainButton: UIButton!
+    @IBOutlet weak var backToHomeButton: UIButton!
+}
