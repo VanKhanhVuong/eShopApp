5986952dfaea802172dabba0bb570d78ef6079f9
Văn Khánh Vương <vankhanhvuong98@gmail.com>
2021-06-03 13:45:28 +0700

    fix: delete icon old and add all icon new to project

--- eShopApp/eShopApp/Screen/AccountScreen/ViewModel/AccountViewModel.swift
+++ eShopApp/eShopApp/Screen/AccountScreen/ViewModel/AccountViewModel.swift
@@ -9,5 +9,5 @@ import UIKit
 
 class AccountViewModel {
     var arrayText: [String] = ["Orders","My Details","Delivery Address","Payment Methods","Promo Cord","Notifications","Help","Abouts"]
-    var arrayImage:[UIImage] = [#imageLiteral(resourceName: "Order3x"),#imageLiteral(resourceName: "Detail3x-1"),#imageLiteral(resourceName: "Address3x"),#imageLiteral(resourceName: "Detail3x-1"),#imageLiteral(resourceName: "Promo3x"),#imageLiteral(resourceName: "Bell3x"),#imageLiteral(resourceName: "help3x"),#imageLiteral(resourceName: "about3x")]
+    var arrayImage:[UIImage] = [#imageLiteral(resourceName: "Order"),#imageLiteral(resourceName: "MyDetails"),#imageLiteral(resourceName: "DeliceryAddress"),#imageLiteral(resourceName: "Payment"),#imageLiteral(resourceName: "PromoCord"),#imageLiteral(resourceName: "Bell"),#imageLiteral(resourceName: "about"),#imageLiteral(resourceName: "help")]
 }
