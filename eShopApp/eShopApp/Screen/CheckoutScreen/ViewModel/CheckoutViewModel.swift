612a8d9454fd301666bd7a7eba7e007e99225e71
Văn Khánh Vương <vankhanhvuong98@gmail.com>
2021-06-09 18:47:56 +0700

    feat(checkout): code checkout screen - Place order success show order accepted.

--- eShopApp/eShopApp/Screen/CheckoutScreen/ViewModel/CheckoutViewModel.swift
+++ eShopApp/eShopApp/Screen/CheckoutScreen/ViewModel/CheckoutViewModel.swift
@@ -20,5 +20,22 @@ protocol CheckoutViewModelEvents: AnyObject {
 class CheckoutViewModel {
     weak var delegate: CheckoutViewModelEvents?
     var api = APIClient()
-    
+    var idOrder: String = ""
+    var totalBill: String = ""
+
+    func createOrder(idUser: String) {
+        let delivery: String = "23/1"
+        let pament: String = "Card"
+        let promoCode: String = "0.2"
+        api.createOrder(idOrder: idOrder, delivery: delivery, pament: pament, promoCode: promoCode, idUser: idUser, totalBill: totalBill){ [weak self] result in
+            switch result {
+            case .success(_):
+                guard let self = self else { return }
+                self.delegate?.gotData()
+            case .failure(let error):
+                self?.delegate?.gotError(messageError: error.rawValue)
+                break
+            }
+        }
+    }
 }
