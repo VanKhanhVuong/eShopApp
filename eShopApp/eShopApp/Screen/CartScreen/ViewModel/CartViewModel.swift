d5a56e7826cd3c58fce94f78f64906861f95f5c1
Văn Khánh Vương <vankhanhvuong98@gmail.com>
2021-06-09 09:34:34 +0700

    feat(cart): rename title screen, displays a message that the user is not logged in.

--- eShopApp/eShopApp/Screen/CartScreen/ViewModel/CartViewModel.swift
+++ eShopApp/eShopApp/Screen/CartScreen/ViewModel/CartViewModel.swift
@@ -6,17 +6,15 @@
 //
 
 import Foundation
-import KeychainAccess
 
 protocol CartViewModelEvents: AnyObject {
     func gotDataCart(messageChangeData: String)
     func gotAmountProduct(amount: String)
-    func gotErrorCart(messageError: ErrorModel)
+    func gotErrorCart(messageError: String)
 }
 
 class CartViewModel {
     var api = APIClient()
-    var keychain = Keychain()
     var messageAddCart: String = ""
     var arrayCart: [Cart] = []
     weak var delegate: CartViewModelEvents?
@@ -32,127 +30,136 @@ class CartViewModel {
         return sum
     }
     
-    func findAmountProduct(productId: String) {
-        let userId = keychain["token"] ?? ""
-        api.checkCartToAPI(productId: productId, userId: userId) { result in
-            var array: [Cart] = []
-            switch result {
-            case .success(let result):
-                if !result.isEmpty {
-                    result.forEach { (cart) in
-                        array.append(cart)
+    func findAmountProduct(productId: String, userId: String) {
+        if !userId.isEmpty {
+            api.checkCartToAPI(productId: productId, userId: userId) { [weak self] result in
+                guard let self = self else { return }
+                switch result {
+                case .success(let amount):
+                    if !amount.isEmpty {
+                        self.delegate?.gotAmountProduct(amount: "\(amount.first?.amount ?? "")")
+                    } else {
+                        self.delegate?.gotAmountProduct(amount: "1")
                     }
-                    if array.count != 0 {
-                        self.delegate?.gotAmountProduct(amount: "\(array.first?.amount ?? "")")
-                    }
-                } else {
-                    self.delegate?.gotAmountProduct(amount: "1")
+                case .failure(let error):
+                    self.delegate?.gotErrorCart(messageError: error.rawValue)
+                    break
                 }
-            case .failure(let error):
-                self.delegate?.gotErrorCart(messageError: error)
-                break
             }
+        } else {
+            delegate?.gotErrorCart(messageError: "Please Login")
         }
     }
     
-    func filterProductCart(productId: String, amount: String, isCart: Bool) {
-        let userId = keychain["token"] ?? ""
-        api.checkCartToAPI(productId: productId, userId: userId) { result in
-            var arrayFilterCart: [Cart] = []
-            switch result {
-            case .success(let result):
-                if !result.isEmpty {
-                    result.forEach { (cart) in
-                        arrayFilterCart.append(cart)
-                    }
-                    if arrayFilterCart.count != 0 {
+    func filterProductCart(productId: String, amount: String, isCart: Bool, userId: String) {
+        if !userId.isEmpty {
+            api.checkCartToAPI(productId: productId, userId: userId) { [weak self] result in
+                guard let self = self else { return }
+                switch result {
+                case .success(let carts):
+                    if !carts.isEmpty {
                         if isCart {
-                            print("Update amount product in Cart View cartId : \(arrayFilterCart.first?.id ?? "") productid: \(arrayFilterCart.first?.productId ?? "") amount: \(amount)")
+                            print("Update amount product in Cart View cartId : \(carts.first?.id ?? "") productid: \(carts.first?.productId ?? "") amount: \(amount)")
                             guard let amountProduct:Int = Int(amount) else { return }
-                            self.updateAmountProduct(idCart: arrayFilterCart.first?.id ?? "", amount: amountProduct)
+                            self.updateAmountProduct(idCart: carts.first?.id ?? "", amount: amountProduct, userId: userId)
                         } else {
-                            guard let amountProduct: Int = Int(arrayFilterCart.first?.amount ?? "") else { return }
-                            print("Update amount product cartId : \(arrayFilterCart.first?.id ?? "") productid: \(productId) amount: \(amountProduct + 1)")
-                            self.updateAmountProduct(idCart: arrayFilterCart.first?.id ?? "", amount: amountProduct + 1)
+                            guard let amountProduct: Int = Int(carts.first?.amount ?? "") else { return }
+                            print("Update amount product cartId : \(carts.first?.id ?? "") productid: \(productId) amount: \(amountProduct + 1)")
+                            self.updateAmountProduct(idCart: carts.first?.id ?? "", amount: amountProduct + 1, userId: userId)
                         }
+                    } else {
+                        guard let amountProduct: Int = Int(amount) else { return }
+                        self.addCart(productId: productId, userId: userId, amount: amountProduct)
                     }
-                } else {
-                    guard let amountProduct: Int = Int(amount) else { return }
-                    self.addCart(productId: productId, userId: userId, amount: amountProduct)
+                case .failure(let error):
+                    self.delegate?.gotErrorCart(messageError: error.rawValue)
+                    break
                 }
-            case .failure(let error):
-                self.delegate?.gotErrorCart(messageError: error)
-                break
             }
+        } else {
+            delegate?.gotErrorCart(messageError: "Please Login")
         }
     }
     
     // Find Cart By UserID
-    func findCart() {
-        let token = keychain["token"] ?? ""
-        arrayCart = []
-        api.findCartToAPI(userId: token) { [weak self] result in
-            guard let self = self else { return }
-            switch result {
-            case .success(let result):
-                if !result.isEmpty {
-                    result.forEach { (cart) in
-                        self.arrayCart.append(cart)
+    func findCart(userId: String) {
+        if !userId.isEmpty {
+            arrayCart.removeAll()
+            api.findCartToAPI(userId: userId) { [weak self] result in
+                guard let self = self else { return }
+                switch result {
+                case .success(let carts):
+                    if !carts.isEmpty {
+                        self.arrayCart = carts
+                        print(self.arrayCart)
+                        self.delegate?.gotDataCart(messageChangeData: "")
                     }
-                    print(self.arrayCart)
-                    self.delegate?.gotDataCart(messageChangeData: "")
+                case .failure(let error):
+                    self.delegate?.gotErrorCart(messageError: error.rawValue)
+                    break
                 }
-            case .failure(let error):
-                self.delegate?.gotErrorCart(messageError: error)
-                break
             }
+        } else {
+            delegate?.gotErrorCart(messageError: "Please Login")
         }
     }
     
     // Add Cart
     func addCart(productId: String, userId: String, amount: Int) {
-        api.addCartToAPI(productId: productId, userId: userId, amount: amount, completionHandler: { [weak self] result in
-            switch result {
-            case .success(let result):
-                guard let self = self else { return }
-                self.messageAddCart = result.status ?? ""
-                self.delegate?.gotDataCart(messageChangeData: "Product added to cart successfully")
-            case .failure(let error):
-                self?.delegate?.gotErrorCart(messageError: error)
-                break
+        if !userId.isEmpty {
+            api.addCartToAPI(productId: productId, userId: userId, amount: amount) { [weak self] result in
+                switch result {
+                case .success(let result):
+                    guard let self = self else { return }
+                    self.messageAddCart = result.status ?? ""
+                    self.delegate?.gotDataCart(messageChangeData: "Product added to cart successfully")
+                case .failure(let error):
+                    self?.delegate?.gotErrorCart(messageError: error.rawValue)
+                    break
+                }
             }
-        })
+        } else {
+            delegate?.gotErrorCart(messageError: "Please Login")
+        }
     }
     
     // Update Cart
-    func updateAmountProduct(idCart: String, amount: Int) {
-        api.updateAmountCartToAPI(id: idCart, amount: amount) { [weak self] result in
-            switch result {
-            case .success(let result):
-                guard let self = self else { return }
-                self.messageAddCart = result.status ?? ""
-                self.findCart()
-                self.delegate?.gotDataCart(messageChangeData: "Change the number of products successfully")
-            case .failure(let error):
-                self?.delegate?.gotErrorCart(messageError: error)
-                break
+    func updateAmountProduct(idCart: String, amount: Int, userId: String) {
+        if !userId.isEmpty {
+            api.updateAmountCartToAPI(id: idCart, amount: amount) { [weak self] result in
+                switch result {
+                case .success(let result):
+                    guard let self = self else { return }
+                    self.messageAddCart = result.status ?? ""
+                    self.findCart(userId: userId)
+                    self.delegate?.gotDataCart(messageChangeData: "Change the number of products successfully")
+                case .failure(let error):
+                    self?.delegate?.gotErrorCart(messageError: error.rawValue)
+                    break
+                }
             }
+        } else {
+            delegate?.gotErrorCart(messageError: "Please Login")
         }
     }
     
     // Delete Cart
-    func deleteCart(idCart: String) {
-        api.deleteProductCartToAPI(id: idCart){ [weak self] result in
-            switch result {
-            case .success(let result):
-                guard let self = self else { return }
-                self.messageAddCart = result.status ?? ""
-                self.findCart()
-                self.delegate?.gotDataCart(messageChangeData: "Product removed from cart successfully")
-            case .failure(let error):
-                self?.delegate?.gotErrorCart(messageError: error)
-                break
+    func deleteCart(idCart: String, userId: String) {
+        if !userId.isEmpty {
+            api.deleteProductCartToAPI(id: idCart){ [weak self] result in
+                switch result {
+                case .success(let result):
+                    guard let self = self else { return }
+                    self.messageAddCart = result.status ?? ""
+                    self.findCart(userId: userId)
+                    self.delegate?.gotDataCart(messageChangeData: "Product removed from cart successfully")
+                case .failure(let error):
+                    self?.delegate?.gotErrorCart(messageError: error.rawValue)
+                    break
+                }
             }
+        } else {
+            delegate?.gotErrorCart(messageError: "Please Login")
         }
     }
 }
