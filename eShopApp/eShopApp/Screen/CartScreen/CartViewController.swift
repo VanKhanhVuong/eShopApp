8526b15131dcec1a76669ec4a990e296c4dbd783
Văn Khánh Vương <vankhanhvuong98@gmail.com>
2021-06-09 16:34:58 +0700

    feat(cart): send order id for checkout model view.

--- eShopApp/eShopApp/Screen/CartScreen/CartViewController.swift
+++ eShopApp/eShopApp/Screen/CartScreen/CartViewController.swift
@@ -14,6 +14,7 @@ class CartViewController: UIViewController {
     @IBOutlet weak var totalPriceLabel: UILabel!
     
     var cartViewModel = CartViewModel()
+    private var checkoutViewController = CheckoutViewController()
     var cartTableViewCell = CartTableViewCell()
     
     override func viewDidLoad() {
@@ -27,9 +28,8 @@ class CartViewController: UIViewController {
     }
     
     @IBAction func checkoutTapped(_ sender: Any) {
-//        let vc = CheckoutViewController()
-//        vc.modalPresentationStyle = .custom
-//        present(vc, animated: true, completion: nil)
+        // func tạo idOrder = userId_Date
+        createOrder()
     }
     
     func setupUIView() {
@@ -43,10 +43,26 @@ class CartViewController: UIViewController {
         checkoutButton.clipsToBounds = true
         checkoutButton.layer.cornerRadius = 15
     }
+    
+    func createOrder() {
+        let date = Date()
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        let minutes = calendar.component(.minute, from: date)
+        let month = calendar.component(.month, from: date)
+        let year = calendar.component(.year, from: date)
+        let idOrder = getUserId() + "\(hour)\(minutes)\(month)\(year)"
+        cartViewModel.updateOrderIdToCart(idOrder: idOrder, userId:getUserId())
+    }
+    
+    func navigationCheckout() {
+        //checkoutViewController.checkoutViewModel.idOrder = cartViewModel.idOrder
+        checkoutViewController.modalPresentationStyle = .custom
+        present(checkoutViewController, animated: true, completion: nil)
+    }
 }
 
-extension CartViewController: UITableViewDelegate {
-}
+extension CartViewController: UITableViewDelegate {}
 
 extension CartViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -67,6 +83,12 @@ extension CartViewController: UITableViewDataSource {
 }
 
 extension CartViewController: CartViewModelEvents {
+    func gotIdOrder() {
+        DispatchQueue.main.async {
+            self.navigationCheckout()
+        }
+    }
+    
     func gotErrorCart(messageError: String) {
         DispatchQueue.main.async {
             self.showAlert(message: messageError)
