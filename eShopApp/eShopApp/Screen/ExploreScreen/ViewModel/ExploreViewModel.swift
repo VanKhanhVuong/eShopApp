2573a4e308b99357b43114ee996e4562b41b5183
Văn Khánh Vương <vankhanhvuong98@gmail.com>
2021-06-10 12:57:43 +0700

    feat(explore): ddd products to cart in Explore screen.

--- eShopApp/eShopApp/Screen/ExploreScreen/ViewModel/ExploreViewModel.swift
+++ eShopApp/eShopApp/Screen/ExploreScreen/ViewModel/ExploreViewModel.swift
@@ -8,21 +8,24 @@
 import UIKit
 
 protocol ExploreViewModelEvents: AnyObject {
-    func gotData()
+    func gotData(isSearch: Bool)
     func gotMessage(message: String)
     func gotError(messageError: ErrorModel)
 }
 
 class ExploreViewModel {
+    var nameCategory: String = ""
     var arrayImageCategory: [UIImage] = []
     var arrayNameCategory: [String] = []
+    var arrayIdCategory: [String] = []
     var arrayColorBackground: [UIColor] = []
-    var arrayProductByName: [Product] = []
+    var arrayProduct: [Product] = []
     var api = APIClient()
     weak var delegate: ExploreViewModelEvents?
     
     func loadItemCategory() {
         arrayImageCategory = [#imageLiteral(resourceName: "tv"),#imageLiteral(resourceName: "Iphone"),#imageLiteral(resourceName: "Laptop"),#imageLiteral(resourceName: "EarPhone"),#imageLiteral(resourceName: "SmartWatch")]
+        arrayIdCategory = ["category001","category002","category003","category005","category006"]
         arrayNameCategory = ["TV","Smart Phone","Laptop","Ear Phone","Smart Watch"]
         arrayColorBackground = [UIColor(red: 230/255, green: 70/255, blue: 63/255, alpha: 1),
                                 UIColor(red: 236/255, green: 235/255, blue: 133/255, alpha: 1),
@@ -30,27 +33,45 @@ class ExploreViewModel {
                                 UIColor(red: 138/255, green: 178/255, blue: 228/255, alpha: 1),
                                 UIColor(red: 210/255, green: 81/255, blue: 91/255, alpha: 1)
         ]
-        self.delegate?.gotData()
+        self.delegate?.gotData(isSearch: true)
     }
     
     func searchProductByName(productName: String) {
-        arrayProductByName = []
+        arrayProduct.removeAll()
         api.getProductByNameFromAPI(productName: productName) { [weak self] result in
             guard let self = self else { return }
             switch result {
-            case .success(let result):
-                if !result.isEmpty {
-                    result.forEach { (products) in
-                        self.arrayProductByName.append(products)
-                        
-                    }
-                    print(self.arrayProductByName)
-                    self.delegate?.gotData()
+            case .success(let products):
+                if !products.isEmpty {
+                    self.arrayProduct = products
+                    print(self.arrayProduct)
+                    self.delegate?.gotData(isSearch: true)
                 } else {
                     self.delegate?.gotMessage(message: "No product found named \(productName)")
                 }
             case .failure(let error):
-                self.arrayProductByName = []
+                self.arrayProduct.removeAll()
+                self.delegate?.gotError(messageError: error)
+            }
+        }
+    }
+    
+    func getProductByCategory(categoryId: String, categoryName: String) {
+        api.getProductByCategoryFromAPI(categoryId: categoryId) {
+            [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let products):
+                if !products.isEmpty {
+                    self.arrayProduct = products
+                    print(self.arrayProduct)
+                    self.nameCategory = categoryName
+                    self.delegate?.gotData(isSearch: false)
+                } else {
+                    self.delegate?.gotMessage(message: "Sorry we don't have \(categoryName) products.")
+                }
+            case .failure(let error):
+                self.arrayProduct.removeAll()
                 self.delegate?.gotError(messageError: error)
             }
         }
